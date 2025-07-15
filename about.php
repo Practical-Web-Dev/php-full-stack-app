4b3c2a4f7af23a4dd70eb4890b219958c3cdcf65
Practical Web Dev <info.practicalwebdev@gmail.com>
2025-07-15 15:51:36 -0700

    add-home-about-html-css

--- about.php
+++ about.php
@@ -1,5 +1,10 @@
 <?php $page_title = "About"; ?>
 <?php include 'includes/head.php' ?>
 <?php include 'includes/header.php' ?>
-<h1>About</h1>
+<main class="main-content-container">
+<h1 class="page-header">About</h1> 
+<img src="img/about.jpg" alt="content" class="featured-img m-bottom">
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium cum est architecto libero adipisci at rem ab ratione quam enim velit unde placeat repellat aliquam laboriosam excepturi quia natus, sunt laudantium magnam aut eaque ut? Explicabo animi, consectetur veritatis eveniet quis a sit libero tempore assumenda unde suscipit tenetur? Dignissimos at recusandae nemo non omnis deserunt fugiat commodi ullam corporis. At autem perspiciatis maxime odio voluptates delectus incidunt! Animi est, doloribus rem aspernatur fugiat assumenda aperiam cum expedita! Maiores recusandae deserunt nobis! Exercitationem nisi ad esse ipsa voluptate. Unde quidem magni aspernatur delectus adipisci voluptates ad distinctio qui placeat quod!</p>
+<a class="btn-link purple-bg" href="http://localhost:8888/login-app/blog.php">Visit Blog</a>
+</main>
 <?php include 'includes/footer.php' ?>
--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php $page_title = "Home"; ?>
 <?php include 'includes/head.php' ?>
 <?php include 'includes/header.php' ?>
-  <h1>Home</h1>
+<section class="showcase">
+  <h1 class="showcase-heading">Welcome to the Content Site</h1>
+  <div class="showcase-link-container">
+    <a class="btn-link purple-bg margin-l-r" href="http://localhost:8888/login-app/login.php">Sign in</a>
+    <a class="btn-link light-blue-bg margin-l-r" href="#">Sign Up</a>
+  </div>
+</section>
+<main class="main-content-container">
+  <img src="img/home-content-image.png" alt="content" class="featured-img">
+  <h2 class="post-heading">The content Site is the Best CMS in the World!</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam iste perferendis quam velit ex fugiat accusamus
+    odit quod accusantium error veritatis explicabo, facilis et laboriosam, autem earum qui ipsum, aperiam temporibus
+    quidem! Non expedita aliquam accusamus iusto quas at odit, praesentium optio facilis distinctio repellat commodi
+    dolore quibusdam aliquid magnam numquam sed. Non maxime adipisci maiores ullam sed voluptatum excepturi laboriosam
+    amet ab natus labore, nesciunt pariatur molestias magnam ex vero aperiam aut alias nobis quam, error est facere esse
+    ad. Itaque, atque maiores! Ratione quidem distinctio consequatur deserunt illo voluptas iure, quo placeat iste
+    maiores laudantium similique aspernatur quaerat ipsa delectus doloremque eius repudiandae culpa. Cumque quod et
+    earum temporibus, exercitationem accusantium eligendi? Consequuntur alias temporibus asperiores ducimus ipsam.
+    Voluptates odit tempora possimus, ipsam velit quis eaque fugit enim aut odio harum facere laborum illum praesentium,
+    est reprehenderit? Magni porro similique asperiores nemo quis aspernatur impedit animi, blanditiis minima!</p>
+  <a class="btn-link purple-bg" href="http://localhost:8888/login-app/blog.php">Visit Blog</a>
+</main>
 <?php include 'includes/footer.php' ?>
